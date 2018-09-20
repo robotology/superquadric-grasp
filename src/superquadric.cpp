46360322ca01fea4bb391d16508d2fe8ea6f9872
Giulia Vezzani <vzzgli@gmail.com>
2018-09-20 15:08:18 +0200

    implicit function cone as suggested by ugo: much more fast

--- src/superquadric.cpp
+++ src/superquadric.cpp
@@ -362,7 +362,9 @@ bool grasping_NLP::get_bounds_info(Ipopt::Index n, Ipopt::Number *x_l, Ipopt::Nu
     double p_dot_p=dot(point, point);
     double p_dot_d=dot(point, d);
 
-    return  ((p_dot_d - 1)) * (-abs(acos(p_dot_d))+theta);
+    //return  ((p_dot_d - 1)) * (-abs(acos(p_dot_d))+theta);
+
+    return  (-p_dot_d + cos(theta));
 
  }
 
